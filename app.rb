8cde0b46fb6644d6871a738b9bd3d7ce98657cd7
Ashley <ashleynjeri7@gmail.com>
2022-10-17 13:24:15 +0300

    Passed test

---
+++ app.rb
@@ -0,0 +1,38 @@
+puts "Hello World!"
+print "Pass this test, please."
+p [1,2,3]
+
+
+
+
+
+
+
+
+# puts is a method that outputs string text
+# using parenthesis is optional
+#puts "Hello world!"
+#puts "Hello world!"
+#puts "Hello world!"
+
+#puts "***************"
+# no line break at the end of the string 
+#print "Hello world!"
+#print "Hello world!"
+#print "Hello world!"
+
+#puts "//************//"
+
+# coercefully converts array to string using .to_s method
+#puts [1, 2, 3]
+
+#puts "***************"
+
+# outputs array as is
+#p [1, 2, 3]
+
+# pp - 'pretty-printing'
+# outputs complex data such as nested arrays & hashes
+#pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+#puts "***************"
